76ef123f1de54f912bbc91f06f26d32186b39e1e
IT Hit <build@ithit.com>
2019-05-29 10:19:48 +0000

    4.2.2812

--- Java/filesystemstorage/src/main/java/com/ithit/webdav/samples/fsstorageservlet/extendedattributes/DefaultExtendedAttribute.java
+++ Java/filesystemstorage/src/main/java/com/ithit/webdav/samples/fsstorageservlet/extendedattributes/DefaultExtendedAttribute.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.file.*;
+import java.nio.file.attribute.FileTime;
 import java.nio.file.attribute.UserDefinedFileAttributeView;
 import java.util.List;
 
@@ -18,9 +19,15 @@ class DefaultExtendedAttribute implements ExtendedAttribute {
      */
     @Override
     public void setExtendedAttribute(String path, String attribName, String attribValue) throws IOException {
+    	FileTime lastWriteTime = Files.getLastModifiedTime(Paths.get(path), LinkOption.NOFOLLOW_LINKS);
+    	
         UserDefinedFileAttributeView view = Files
                 .getFileAttributeView(Paths.get(path), UserDefinedFileAttributeView.class);
         view.write(attribName, Charset.defaultCharset().encode(attribValue));
+        
+        // File modification date should not change when locking and unlocking. Otherwise client application may think that the file was changed.
+        // Preserve last modification date.
+        Files.setLastModifiedTime(Paths.get(path), lastWriteTime); 
     }
 
     /**
