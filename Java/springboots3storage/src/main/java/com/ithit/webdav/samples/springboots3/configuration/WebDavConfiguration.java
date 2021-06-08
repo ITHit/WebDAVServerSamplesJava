91223d45bae36c39a20aad98fdf37db1c18cd3da
IT Hit <build@ithit.com>
2021-06-08 21:58:49 +0300

    5.0.5151

--- Java/springboots3storage/src/main/java/com/ithit/webdav/samples/springboots3/configuration/WebDavConfiguration.java
+++ Java/springboots3storage/src/main/java/com/ithit/webdav/samples/springboots3/configuration/WebDavConfiguration.java
@@ -18,7 +18,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.context.annotation.RequestScope;
-import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -61,11 +60,6 @@ public class WebDavConfiguration extends WebMvcConfigurationSupport implements W
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
-    }
-
-    @Bean
-    public RequestContextListener requestContextListener() {
-        return new RequestContextListener();
     }
 
     @Override
